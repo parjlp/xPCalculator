b80d70267f8c9067a5253df2aceb6e3f4b643945
parjlp <110988133+parjlp@users.noreply.github.com>
2026-03-27 13:04:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-import streamlit_authenticator as stauth
 st.set_page_config(layout="wide")
 st.title(body="National League North Justice Tables")
 

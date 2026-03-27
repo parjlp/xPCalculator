ea3dbfc0216adc27bcd93aef4cf35ba92d7fa4d1
parjlp <110988133+parjlp@users.noreply.github.com>
2026-03-27 13:10:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import numpy as np
 import random
 st.set_page_config(layout="wide")
 st.title(body="National League North Justice Tables")
-
+csv_path = "data/justice_table.csv"
 
 col1, col2 = st.columns([1,4])
 
 
 def refresh(data):
     if user_input == "Expected Points Per Game":
-        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = pd.read_csv(csv_path)
         jt = jt[["Team", "Expected Points Per Game"]]
         jt_disp = jt.sort_values(by="Expected Points Per Game", ascending=False)
         jt_disp.insert(0, "Position", list(range(len(jt_disp))))
@@ -19,7 +19,7 @@ def refresh(data):
         with col2:
             st.dataframe(data=jt_disp, hide_index=True, height='content')
     elif user_input == "Expected Goals Per Game":
-        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = pd.read_csv(csv_path)
         jt = jt[["Team", "Expected Goals Per Game"]]
         jt_disp = jt.sort_values(by="Expected Goals Per Game", ascending=False)
         jt_disp.insert(0, "Position", list(range(len(jt_disp))))
@@ -27,7 +27,7 @@ def refresh(data):
         with col2:
             st.dataframe(data=jt_disp, hide_index=True, height='content')
     elif user_input == "Expected Win %":
-        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = pd.read_csv(csv_path)
         jt = jt[["Team", "Expected Win %"]]
         jt_disp = jt.sort_values(by="Expected Win %", ascending=False)
         jt_disp.insert(0, "Position", list(range(len(jt_disp))))
@@ -35,7 +35,7 @@ def refresh(data):
         with col2:
             st.dataframe(data=jt, hide_index=True, height='content')
     elif user_input == "Expected Loss %":
-        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = pd.read_csv(csv_path)
         jt = jt[["Team", "Expected Loss %"]]
         jt_disp = jt.sort_values(by="Expected Loss %", ascending=False)
         jt_disp.insert(0, "Position", list(range(len(jt_disp))))
@@ -43,7 +43,7 @@ def refresh(data):
         with col2:
             st.dataframe(data=jt, hide_index=True, height='content')
     elif user_input == "Expected Draw %":
-        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = pd.read_csv(csv_path)
         jt = jt[["Team", "Expected Draw %"]]
         jt_disp = jt.sort_values(by="Expected Draw %", ascending=False)
         jt_disp.insert(0, "Position", list(range(len(jt_disp))))

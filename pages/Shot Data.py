d07d97bcf5372a65e5ddf6994b69928b38ba6463
parjlp <110988133+parjlp@users.noreply.github.com>
2026-03-27 13:15:48 +0000

    Update Shot Data.py

--- pages/Shot Data.py
+++ pages/Shot Data.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 st.title(body="National League North All Shot Data")
 st.write("This is all of the raw data, I'm working on analysing it and visualising it in a useful way")
 
-shots = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/master_shots.csv")
+shots = pd.read_csv(master_shots.csv")
 shots.insert(0, "Position", list(range(len(shots))))
 shots["Position"] = shots["Position"] + 1
 st.dataframe(data=shots, hide_index=True, height='content')

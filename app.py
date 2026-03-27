4d7238651f2dc0a6ff22a02e8d148c1f923db61a
parjlp <110988133+parjlp@users.noreply.github.com>
2026-03-27 12:52:09 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+import streamlit_authenticator as stauth
+st.set_page_config(layout="wide")
+st.title(body="National League North Justice Tables")
+
+
+col1, col2 = st.columns([1,4])
+
+
+def refresh(data):
+    if user_input == "Expected Points Per Game":
+        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = jt[["Team", "Expected Points Per Game"]]
+        jt_disp = jt.sort_values(by="Expected Points Per Game", ascending=False)
+        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+        jt_disp["Position"] = jt_disp["Position"] + 1
+        with col2:
+            st.dataframe(data=jt_disp, hide_index=True, height='content')
+    elif user_input == "Expected Goals Per Game":
+        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = jt[["Team", "Expected Goals Per Game"]]
+        jt_disp = jt.sort_values(by="Expected Goals Per Game", ascending=False)
+        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+        jt_disp["Position"] = jt_disp["Position"] + 1
+        with col2:
+            st.dataframe(data=jt_disp, hide_index=True, height='content')
+    elif user_input == "Expected Win %":
+        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = jt[["Team", "Expected Win %"]]
+        jt_disp = jt.sort_values(by="Expected Win %", ascending=False)
+        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+        jt_disp["Position"] = jt_disp["Position"] + 1
+        with col2:
+            st.dataframe(data=jt, hide_index=True, height='content')
+    elif user_input == "Expected Loss %":
+        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = jt[["Team", "Expected Loss %"]]
+        jt_disp = jt.sort_values(by="Expected Loss %", ascending=False)
+        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+        jt_disp["Position"] = jt_disp["Position"] + 1
+        with col2:
+            st.dataframe(data=jt, hide_index=True, height='content')
+    elif user_input == "Expected Draw %":
+        jt = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/justice_table.csv")
+        jt = jt[["Team", "Expected Draw %"]]
+        jt_disp = jt.sort_values(by="Expected Draw %", ascending=False)
+        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+        jt_disp["Position"] = jt_disp["Position"] + 1
+        with col2:
+            st.dataframe(data=jt_disp, hide_index=True, height='content')
+
+
+
+
+
+
+
+with col1:    
+    data_options = ["Expected Points Per Game", "Expected Goals Per Game", "Expected Win %", "Expected Loss %", "Expected Draw %"]
+    user_input = st.radio(label="Select Dataset", options=data_options)
+    st.button(label="Show Selected Data", on_click=refresh, args=[user_input])
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pages/Shot Data.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+import streamlit_authenticator as stauth
+st.set_page_config(layout="wide")
+st.title(body="National League North All Shot Data")
+st.write("This is all of the raw data, I'm working on analysing it and visualising it in a useful way")
+
+shots = pd.read_csv("C:/Users/parjl/Desktop/Football App/SofaScore Data/master_shots.csv")
+shots.insert(0, "Position", list(range(len(shots))))
+shots["Position"] = shots["Position"] + 1
+st.dataframe(data=shots, hide_index=True, height='content')
+

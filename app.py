bf343cf233c9ac563ac47b41740e48c694a5338b
parjlp <110988133+parjlp@users.noreply.github.com>
2026-03-29 21:31:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,47 +9,46 @@ csv_path = "justice_table.csv"
 col1, col2 = st.columns([1,4])
 
 
-def refresh(data):
-    if user_input == "Expected Points Per Game":
-        jt = pd.read_csv(csv_path)
-        jt = jt[["Team", "Expected Points Per Game"]]
-        jt_disp = jt.sort_values(by="Expected Points Per Game", ascending=False)
-        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
-        jt_disp["Position"] = jt_disp["Position"] + 1
-        with col2:
-            st.dataframe(data=jt_disp, hide_index=True, height='content')
-    elif user_input == "Expected Goals Per Game":
-        jt = pd.read_csv(csv_path)
-        jt = jt[["Team", "Expected Goals Per Game"]]
-        jt_disp = jt.sort_values(by="Expected Goals Per Game", ascending=False)
-        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
-        jt_disp["Position"] = jt_disp["Position"] + 1
-        with col2:
-            st.dataframe(data=jt_disp, hide_index=True, height='content')
-    elif user_input == "Expected Win %":
-        jt = pd.read_csv(csv_path)
-        jt = jt[["Team", "Expected Win %"]]
-        jt_disp = jt.sort_values(by="Expected Win %", ascending=False)
-        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
-        jt_disp["Position"] = jt_disp["Position"] + 1
-        with col2:
-            st.dataframe(data=jt, hide_index=True, height='content')
-    elif user_input == "Expected Loss %":
-        jt = pd.read_csv(csv_path)
-        jt = jt[["Team", "Expected Loss %"]]
-        jt_disp = jt.sort_values(by="Expected Loss %", ascending=False)
-        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
-        jt_disp["Position"] = jt_disp["Position"] + 1
-        with col2:
-            st.dataframe(data=jt, hide_index=True, height='content')
-    elif user_input == "Expected Draw %":
-        jt = pd.read_csv(csv_path)
-        jt = jt[["Team", "Expected Draw %"]]
-        jt_disp = jt.sort_values(by="Expected Draw %", ascending=False)
-        jt_disp.insert(0, "Position", list(range(len(jt_disp))))
-        jt_disp["Position"] = jt_disp["Position"] + 1
-        with col2:
-            st.dataframe(data=jt_disp, hide_index=True, height='content')
+if user_input == "Expected Points Per Game":
+    jt = pd.read_csv(csv_path)
+    jt = jt[["Team", "Expected Points Per Game"]]
+    jt_disp = jt.sort_values(by="Expected Points Per Game", ascending=False)
+    jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+    jt_disp["Position"] = jt_disp["Position"] + 1
+    with col2:
+        st.dataframe(data=jt_disp, hide_index=True, height='content')
+elif user_input == "Expected Goals Per Game":
+    jt = pd.read_csv(csv_path)
+    jt = jt[["Team", "Expected Goals Per Game"]]
+    jt_disp = jt.sort_values(by="Expected Goals Per Game", ascending=False)
+    jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+    jt_disp["Position"] = jt_disp["Position"] + 1
+    with col2:
+        st.dataframe(data=jt_disp, hide_index=True, height='content')
+elif user_input == "Expected Win %":
+    jt = pd.read_csv(csv_path)
+    jt = jt[["Team", "Expected Win %"]]
+    jt_disp = jt.sort_values(by="Expected Win %", ascending=False)
+    jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+    jt_disp["Position"] = jt_disp["Position"] + 1
+    with col2:
+        st.dataframe(data=jt, hide_index=True, height='content')
+elif user_input == "Expected Loss %":
+    jt = pd.read_csv(csv_path)
+    jt = jt[["Team", "Expected Loss %"]]
+    jt_disp = jt.sort_values(by="Expected Loss %", ascending=False)
+    jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+    jt_disp["Position"] = jt_disp["Position"] + 1
+    with col2:
+        st.dataframe(data=jt, hide_index=True, height='content')
+elif user_input == "Expected Draw %":
+    jt = pd.read_csv(csv_path)
+    jt = jt[["Team", "Expected Draw %"]]
+    jt_disp = jt.sort_values(by="Expected Draw %", ascending=False)
+    jt_disp.insert(0, "Position", list(range(len(jt_disp))))
+    jt_disp["Position"] = jt_disp["Position"] + 1
+    with col2:
+        st.dataframe(data=jt_disp, hide_index=True, height='content')
 
 
 with col1:

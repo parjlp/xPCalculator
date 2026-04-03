6e4c95276dc6f14484355f96421c58bbc091e664
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-03 10:57:11 +0100

    Update Team Post Shot xG.py

--- pages/Team Post Shot xG.py
+++ pages/Team Post Shot xG.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 
-teams_df = pd.read_csv("C:/Users/parjl/footballapp/data/team_total.csv")
+teams_df = pd.read_csv("team_total.csv")
 
 teams_disp = teams_df.sort_values(by="Team Total Impact on Post Shot xG", ascending=False)
 teams_disp.insert(0, "League Rank", list(range(len(teams_disp))))

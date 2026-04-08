c75f58b57ece202234a28d93189e6dabd7cdc9d7
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 11:42:48 +0100

    Update Expected Points By Team Without Penalties.py

--- pages/Expected Points By Team Without Penalties.py
+++ pages/Expected Points By Team Without Penalties.py
@@ -4,29 +4,28 @@ import numpy as np
 import random
 from pathlib import Path
 
+from pathlib import Path
+
 BASE_DIR = Path(__file__).parent
 DATA_DIR = BASE_DIR / "data"
 
-
 st.set_page_config(layout="wide")
-st.title(body="National League North Expected Points By Team")
+st.title(body="National League North Expected Points By Team Without Penalties")
 
-results = pd.read_csv(DATA_DIR / "without_penalties" / "results.csv"))
+results = pd.read_csv(DATA_DIR / "without_penalties" / "results.csv")
 teams = results["Home Team"].unique()
 
-col1, col2 = st.columns([1,4])
+col1, col2 = st.columns([1, 4])
+
 with col1:
-    team_select = st.selectbox(label="Select Dataset", options=teams, index=False, accept_new_options=False)
+    team_select = st.selectbox(label="Select Team", options=teams, index=0)
 
-df_team = results[(results["Home Team"] == team_select) |
-                    (results["Away Team"] == team_select)]
+if team_select:
+    df_team = results[(results["Home Team"] == team_select) |
+                      (results["Away Team"] == team_select)].copy()
+    df_team["Date"] = pd.to_datetime(df_team["Date"], format="%d/%m/%Y")
+    df_team_disp = df_team.sort_values(by="Date")
 
-df_team.loc[:, "Date"] = pd.to_datetime(df_team["Date"], format="%d/%m/%Y")
-
-df_team_disp = df_team.sort_values(by="Date")
-with col2:
-    df_final = df_team_disp[["Home Team", "Away Team", "Home Team xP", "Away Team xP", "Home Team xG", "Away Team xG"]]
-    # df_final.insert(0, "Link To Game", "View Match")
-
-        
-    st.dataframe(df_final, hide_index=True)
+    with col2:
+        df_final = df_team_disp[["Home Team", "Away Team", "Home Team xP", "Away Team xP", "Home Team xG", "Away Team xG"]]
+        st.dataframe(df_final, hide_index=True)

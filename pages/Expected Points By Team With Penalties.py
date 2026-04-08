d4a25fa0e31a7683f3d8093676740a00fffc6bdb
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 12:17:23 +0100

    Update Expected Points By Team With Penalties.py

--- pages/Expected Points By Team With Penalties.py
+++ pages/Expected Points By Team With Penalties.py
@@ -12,7 +12,7 @@ DATA_DIR = BASE_DIR / "data"
 st.set_page_config(layout="wide")
 st.title(body="National League North Expected Points By Team")
 
-results = pd.read_csv(DATA_DIR / "with_penalties" / "with_penalties" / "results.csv")
+results = pd.read_csv(DATA_DIR / "with_penalties" / "other_data" / "results.csv")
 teams = results["Home Team"].unique()
 
 col1, col2 = st.columns([1, 4])

33a68a05a2883cd74e6774e9a31717d5dc5f7dab
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 11:41:00 +0100

    Update Player Post Shot Without Penalties xG.py

--- pages/Player Post Shot Without Penalties xG.py
+++ pages/Player Post Shot Without Penalties xG.py
@@ -3,18 +3,15 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
+from pathlib import Path
+
 BASE_DIR = Path(__file__).parent
 DATA_DIR = BASE_DIR / "data"
 
-#st.set_page_config(layout="wide")
-
-
-ave_df = pd.read_csv("C:/Users/parjl/footballapp/data/without_penalties/player_total.csv")
-
+csv_in = DATA_DIR / "without_penalties" / "results.csv"
+ave_df = pd.read_csv(csv_in)
 ave_df = ave_df[["Player Name", "Player Team", "Player Total Impact on Post Shot xG"]]
-
-
 ave_disp = ave_df.sort_values(by="Player Total Impact on Post Shot xG", ascending=False)
-ave_disp.insert(0, "League Rank", list(range(len(ave_disp))))
-ave_disp["League Rank"] = ave_disp["League Rank"] + 1
-st.dataframe(ave_disp, hide_index=True, height="content")
+ave_disp.insert(0, "League Rank", range(1, len(ave_disp) + 1))
+
+st.dataframe(ave_disp, hide_index=True)

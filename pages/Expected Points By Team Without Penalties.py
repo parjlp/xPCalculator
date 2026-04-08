1e718256fd1b560fecd95ca52451dd71d78bad39
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 11:24:50 +0100

    Update Expected Points By Team Without Penalties.py

--- pages/Expected Points By Team Without Penalties.py
+++ pages/Expected Points By Team Without Penalties.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-import streamlit_authenticator as stauth
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+DATA_DIR = BASE_DIR / "data"
+
+
 st.set_page_config(layout="wide")
 st.title(body="National League North Expected Points By Team")
 

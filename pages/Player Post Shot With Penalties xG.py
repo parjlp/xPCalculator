8852e9a96d648f8beab2c355c485549d6884d68f
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 11:26:08 +0100

    Update Player Post Shot With Penalties xG.py

--- pages/Player Post Shot With Penalties xG.py
+++ pages/Player Post Shot With Penalties xG.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import random
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+DATA_DIR = BASE_DIR / "data"
 
 #st.set_page_config(layout="wide")
 

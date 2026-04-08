429d45bc455fd4c6177542970f9bd7bc4d3be80b
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 12:31:53 +0100

    Update Player Post Shot Without Penalties xG.py

--- pages/Player Post Shot Without Penalties xG.py
+++ pages/Player Post Shot Without Penalties xG.py
@@ -8,7 +8,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent  # goes up from pages/ to project root
 DATA_DIR = BASE_DIR / "data"
 
-csv_in = DATA_DIR / "without_penalties" / "other_data" / "justice_table.csv"
+csv_in = DATA_DIR / "without_penalties" / "player_stats" / "player_total.csv"
 ave_df = pd.read_csv(csv_in)
 ave_df = ave_df[["Player Name", "Player Team", "Player Total Impact on Post Shot xG"]]
 ave_disp = ave_df.sort_values(by="Player Total Impact on Post Shot xG", ascending=False)

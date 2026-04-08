8f389e8d77aaee155c134db13f1ef89e02cf83e6
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-08 12:02:44 +0100

    Update Team Post Shot With Penalties xG.py

--- pages/Team Post Shot With Penalties xG.py
+++ pages/Team Post Shot With Penalties xG.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent
+
+BASE_DIR = Path(__file__).parent.parent
 DATA_DIR = BASE_DIR / "data"
 
-teams_df = pd.read_csv("C:/Users/parjl/footballapp/data/with_penalties/team_total.csv")
-
+teams_df = pd.read_csv(DATA_DIR / "with_penalties" / "team_total.csv")
 teams_disp = teams_df.sort_values(by="Team Total Impact on Post Shot xG", ascending=False)
-teams_disp.insert(0, "League Rank", list(range(len(teams_disp))))
-teams_disp["League Rank"] = teams_disp["League Rank"] + 1
-st.dataframe(teams_disp, hide_index=True, height="content")
+teams_disp.insert(0, "League Rank", range(1, len(teams_disp) + 1))
 
+st.dataframe(teams_disp, hide_index=True)
 
 

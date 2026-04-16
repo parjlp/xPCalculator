9025758cfa91ff23fbe1b3483a8995ce10d2d8fd
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-16 19:33:30 +0100

    Update 4Team Post Shot.py

--- pages/4Team Post Shot.py
+++ pages/4Team Post Shot.py
@@ -8,16 +8,7 @@ DATA_DIR = BASE_DIR / "data"
 st.set_page_config(layout="wide")
 st.title("National League North Team Shot Stats")
 
-penalty_toggle = st.radio(
-    label="Select Dataset",
-    options=["With Penalties", "Without Penalties"],
-    horizontal=True
-)
-
-if penalty_toggle == "With Penalties":
-    data_path = DATA_DIR / "with_penalties" / "team_stats" / "team_total.csv"
-else:
-    data_path = DATA_DIR / "without_penalties" / "team_stats" / "team_total.csv"
+data_path = DATA_DIR / "team_total.csv"
 
 teams_df = pd.read_csv(data_path)
 teams_disp = teams_df.sort_values(by="Team Total Impact on Post Shot xG", ascending=False)

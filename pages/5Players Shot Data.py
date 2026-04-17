11e560826d556e3c99259d673194673089071d38
parjlp <110988133+parjlp@users.noreply.github.com>
2026-04-17 08:07:10 +0100

    Update 5Players Shot Data.py

--- pages/5Players Shot Data.py
+++ pages/5Players Shot Data.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent  # goes up from pages/ to project root
-DATA_DIR = BASE_DIR 
+DATA_DIR = BASE_DIR / "data"
 
 st.set_page_config(layout="wide")
 
